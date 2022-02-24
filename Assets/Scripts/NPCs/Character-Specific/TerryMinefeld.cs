df1b85d2a223c7ee23d0eb190f4e1c2a5d7b42eb
Conor C <CBJCalam@gmail.com>
2022-02-24 22:53:30 +1100

    Terry Stand-up dialogue implemented in test-scene

--- Assets/Scripts/NPCs/Character-Specific/TerryMinefeld.cs
+++ Assets/Scripts/NPCs/Character-Specific/TerryMinefeld.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DialogueEditor;
 
 public class TerryMinefeld : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int headTurnSpeed = 3;
+
+    public int terryStandUpCounter = 0;
+
+    public Transform neckJoint;
+
+    public GameObject terryIdlePos;
+    private GameObject playerCamera;
+
+    public bool lookAtPlayer = false;
+
+    private void Awake()
     {
-        
+        playerCamera = GameObject.FindGameObjectWithTag("PlayerCamera");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!lookAtPlayer)
+        {
+            var lookPos = terryIdlePos.transform.position - neckJoint.position;
+            var rotation = Quaternion.LookRotation(lookPos);
+            neckJoint.transform.rotation = Quaternion.Slerp(neckJoint.rotation, rotation, headTurnSpeed * Time.deltaTime);
+        }
+        else
+        {
+            var lookPos = playerCamera.transform.position - neckJoint.position;
+            var rotation = Quaternion.LookRotation(lookPos);
+            neckJoint.transform.rotation = Quaternion.Slerp(neckJoint.rotation, rotation, headTurnSpeed * Time.deltaTime);
+        }
+    }
+
+    public void LookAtPlayer()
+    {
+        if (!lookAtPlayer)
+            lookAtPlayer = true;
+        else
+            lookAtPlayer = false;
+    }
+
+
+    public void GetConversationVariables()
+    {
+        ConversationManager.Instance.SetInt("terryStandUpCounter", terryStandUpCounter);
+    }
+
+    public void SetConversationVariables()
+    {
+        terryStandUpCounter = ConversationManager.Instance.GetInt("terryStandUpCounter");
+    }
+
+    public void TerryStandUpCounterIncrease()
+    {
+        terryStandUpCounter++;
+        ConversationManager.Instance.SetInt("terryStandUpCounter", terryStandUpCounter);
     }
 }
